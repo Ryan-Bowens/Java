fbfab814223111d5f6be73d2adb0ccc39418308a
Ryan_Bowens <bowensryan@gmail.com>
2023-11-27 15:04:05 -0800

    FirstJava

---
+++ PracticeAssignments/FirstJava.java
@@ -0,0 +1,9 @@
+public class FirstJava{
+    public static void main(String[] arg){
+        System.out.println("My name is Coding Dojo");
+        int age = 100;
+        String hometown = "Burbank, CA";
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
